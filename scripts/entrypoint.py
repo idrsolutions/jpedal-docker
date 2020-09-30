769727dbbef27955eb2f548fee751a1375016df7
Kieran <kieran@idrsolutions.com>
2020-09-30 09:53:15 +0100

    Update script to require war file to be mounted

--- scripts/entrypoint.py
+++ scripts/entrypoint.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import os
 import subprocess
-from urllib import parse
-from urllib import request
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 TEMPLATE_DIRECTORY = 'templates'
@@ -14,48 +12,6 @@ def generate_configs():
         t = env.get_template(template_name)
         with open('conf/' + template_name, 'w') as conf:
             conf.write(t.render(https_enabled=ssl_certificates_provided(), auth_enabled=auth_credentials_provided()))
-
-
-def download_trial_war():
-    print('Downloading jpedal trial...')
-    token = os.environ['TOKEN']
-    url = 'https://files.idrsolutions.com/dl/jpedal/trial/jpedal-microservice.war?token=' + token
-    with request.urlopen(url) as response:
-        handle_response(response)
-
-
-def download_full_war(url):
-    username = os.environ['LICENSE_USERNAME']
-    password = os.environ['LICENSE_PASSWORD']
-    data = parse.urlencode({'username': username, 'password': password}).encode('ascii')
-    with request.urlopen(url, data) as response:
-        handle_response(response)
-
-
-def handle_response(response):
-    if response.status == 200:
-        with open('/usr/local/tomcat/webapps/ROOT.war', 'wb') as f:
-            f.write(response.read())
-        print('Download successful')
-    else:
-        print('Download failed')
-        print(response.read().decode())
-
-
-def download_jpedal():
-    print('Downloading jpedal...')
-    download_full_war('https://files.idrsolutions.com/dl/jpedal/full/jpedal-microservice.war')
-
-def is_trial():
-    return 'TOKEN' in os.environ
-
-
-def is_full():
-    return all(x in os.environ for x in ['LICENSE_USERNAME', 'LICENSE_PASSWORD', 'PRODUCT'])
-
-
-def new_war_required():
-    return ('REDOWNLOAD' in os.environ) or not war_exists()
 
 
 def war_exists():
@@ -72,25 +28,6 @@ def auth_credentials_provided():
     return all(x in os.environ for x in ['ACCESS_USERNAME', 'ACCESS_PASSWORD'])
 
 
-if new_war_required():
-    if is_trial() and is_full():
-        print('Mixing trial TOKENs and full-version LICENSE_USERNAME/LICENSE_PASSWORD combination is not supported.')
-        print('If you have purchased a license, please remove the TOKEN argument to use the full version.')
-        print('If you wish to trial jpedal, please remove any LICENSE_USERNAME, LICENSE_PASSWORD or PRODUCT argument.')
-        exit(2)
-    elif is_full():
-        product = os.environ['PRODUCT'].lower()
-        if product == 'jpedal':
-            download_jpedal()
-        else:
-            print('Error: Unrecognised product "' + product + '".')
-            print('Valid options are "jpedal".')
-            exit(2)
-    elif is_trial():
-        download_trial_war()
-    else:
-        print('Please provide a valid trial token or username/password/product combination to download jpedal')
-
 if war_exists():
     generate_configs()
     try:
@@ -98,7 +35,9 @@ if war_exists():
     except KeyboardInterrupt:
         exit()
 else:
-    print('Error: jpedal WAR file is missing.')
+    print('Error: JPedal WAR file is missing.')
+    print('Please mount your JPedal Microservice, details can be found at the following link.')
+    print('https://docs.idrsolutions.com/jpedal/docker-deployment/')
     exit(1)
 
 
